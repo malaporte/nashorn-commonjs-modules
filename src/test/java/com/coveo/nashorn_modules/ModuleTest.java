b3284ead40985f22d840ff5be4c451ffb9906d81
Martin Laporte <mlaporte@coveo.com>
2016-03-25 13:09:34 +0100

    Rename Require class to Module.

--- src/test/java/com/coveo/nashorn_modules/ModuleTest.java
+++ src/test/java/com/coveo/nashorn_modules/ModuleTest.java
@@ -22,7 +22,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class RequireTest {
+public class ModuleTest {
     @Mock
     Folder root;
     @Mock
