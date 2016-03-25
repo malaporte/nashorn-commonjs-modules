46237badcae002c6dad9a8875cfdf97eef3bfa15
Martin Laporte <mlaporte@coveo.com>
2016-03-25 13:39:43 +0100

    Add support for folders backed by filesystem.

---
+++ src/main/java/com/coveo/nashorn_modules/FilesystemFolder.java
@@ -0,0 +1,49 @@
+package com.coveo.nashorn_modules;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FilesystemFolder extends AbstractFolder {
+    private File root;
+    private String encoding = "UTF-8";
+
+    private FilesystemFolder(File root, Folder parent, String path, String encoding) {
+        super(parent, path);
+        this.root = root;
+        this.encoding = encoding;
+    }
+
+    @Override
+    public String getFile(String name) {
+        File file = new File(root, name);
+
+        try {
+            try (FileInputStream stream = new FileInputStream(file)) {
+                return IOUtils.toString(stream, encoding);
+            }
+        } catch (FileNotFoundException ex) {
+            return null;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    public Folder getFolder(String name) {
+        File folder = new File(root, name);
+        if (!folder.exists()) {
+            return null;
+        }
+
+        return new FilesystemFolder(folder, this, getPath() + name + "/", encoding);
+    }
+
+    public static FilesystemFolder create(File root, String encoding) {
+        File absolute = root.getAbsoluteFile();
+        return new FilesystemFolder(absolute, null, absolute.getPath() + "/", encoding);
+    }
+}
---
+++ src/test/java/com/coveo/nashorn_modules/FilesystemFolderTest.java
@@ -0,0 +1,66 @@
+package com.coveo.nashorn_modules;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import javax.script.ScriptEngineManager;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+public class FilesystemFolderTest {
+    private File file = new File("src/test/resources/com/coveo/nashorn_modules/test1");
+    private FilesystemFolder root = FilesystemFolder.create(file, "UTF-8");
+
+    @Test
+    public void rootFolderHasTheExpectedProperties() {
+        assertTrue(root.getPath().startsWith("/"));
+        assertTrue(root.getPath().endsWith("/src/test/resources/com/coveo/nashorn_modules/test1/"));
+        assertNull(root.getParent());
+    }
+
+    @Test
+    public void getFileReturnsTheContentOfTheFileWhenItExists() {
+        assertTrue(root.getFile("foo.js").contains("foo"));
+    }
+
+    @Test
+    public void getFileReturnsNullWhenFileDoesNotExists() {
+        assertNull(root.getFile("invalid"));
+    }
+
+    @Test
+    public void getFolderReturnsAnObjectWithTheExpectedProperties() {
+        Folder sub = root.getFolder("subdir");
+        assertTrue(sub.getPath().startsWith("/"));
+        assertTrue(sub.getPath().endsWith("/src/test/resources/com/coveo/nashorn_modules/test1/subdir/"));
+        assertSame(root, sub.getParent());
+        Folder subsub = sub.getFolder("subsubdir");
+        assertTrue(subsub.getPath().startsWith("/"));
+        assertTrue(subsub.getPath().endsWith("/src/test/resources/com/coveo/nashorn_modules/test1/subdir/subsubdir/"));
+        assertSame(sub, subsub.getParent());
+    }
+
+    @Test
+    public void getFolderReturnsNullWhenFolderDoesNotExist() {
+        assertNull(root.getFolder("invalid"));
+    }
+
+    @Test
+    public void getFileCanBeUsedOnSubFolderIfFileExist() {
+        assertTrue(root.getFolder("subdir").getFile("bar.js").contains("bar"));
+    }
+
+    @Test
+    public void filesystemFolderWorksWhenUsedForReal() throws Throwable {
+        NashornScriptEngine engine = (NashornScriptEngine) new ScriptEngineManager().getEngineByName("nashorn");
+        Module.registerMainRequire(engine, root);
+        assertEquals("spam", engine.eval("require('./foo').bar.spam.spam"));
+    }
+}
