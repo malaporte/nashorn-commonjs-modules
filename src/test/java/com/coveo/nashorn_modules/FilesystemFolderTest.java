51e5a21e306e4fe8e634c4b874ad0d0d823b70c0
arisone <arisone@users.noreply.github.com>
2017-11-23 14:01:22 +0100

    Create path using File.separator instead of "/" (#31)

--- src/test/java/com/coveo/nashorn_modules/FilesystemFolderTest.java
+++ src/test/java/com/coveo/nashorn_modules/FilesystemFolderTest.java
@@ -17,10 +17,15 @@ public class FilesystemFolderTest {
   private File file = new File("src/test/resources/com/coveo/nashorn_modules/test1");
   private FilesystemFolder root = FilesystemFolder.create(file, "UTF-8");
 
+  private File subfile = new File(file, "subdir");
+  private File subsubfile = new File(subfile, "subsubdir");
+
+  private String rootPath = file.getAbsolutePath().substring(0, file.getAbsolutePath().indexOf(File.separator));
+
   @Test
   public void rootFolderHasTheExpectedProperties() {
-    assertTrue(root.getPath().startsWith("/"));
-    assertTrue(root.getPath().endsWith("/src/test/resources/com/coveo/nashorn_modules/test1/"));
+    assertTrue(root.getPath().startsWith(rootPath));
+    assertTrue(root.getPath().endsWith(file.getPath() + File.separator));
     assertNull(root.getParent());
   }
 
@@ -37,16 +42,12 @@ public class FilesystemFolderTest {
   @Test
   public void getFolderReturnsAnObjectWithTheExpectedProperties() {
     Folder sub = root.getFolder("subdir");
-    assertTrue(sub.getPath().startsWith("/"));
-    assertTrue(
-        sub.getPath().endsWith("/src/test/resources/com/coveo/nashorn_modules/test1/subdir/"));
+    assertTrue(sub.getPath().startsWith(rootPath));
+    assertTrue(sub.getPath().endsWith(subfile.getPath() + File.separator));
     assertSame(root, sub.getParent());
     Folder subsub = sub.getFolder("subsubdir");
-    assertTrue(subsub.getPath().startsWith("/"));
-    assertTrue(
-        subsub
-            .getPath()
-            .endsWith("/src/test/resources/com/coveo/nashorn_modules/test1/subdir/subsubdir/"));
+    assertTrue(subsub.getPath().startsWith(rootPath));
+    assertTrue(subsub.getPath().endsWith(subsubfile.getPath() + File.separator));
     assertSame(sub, subsub.getParent());
   }
 
